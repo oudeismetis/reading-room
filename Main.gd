0690e766ac7fd3b4e7162b745d360848419c919e
oudeismetis <oudeis.metis@gmail.com>
2021-05-14 16:19:39 -0400

    Log event when Google Cardboard button pressed

--- Main.gd
+++ Main.gd
@@ -13,3 +13,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _input(_event):
+	if Input.is_action_just_pressed("click"):
+		print("Click pressed!!!")
